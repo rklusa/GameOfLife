77781958246e9e648d16c5e767eaa7a0e6cc8fa2
rklusa <ryan.klusacek@hotmail.ca>
2023-05-04 15:44:29 -0400

    Add project files.

---
+++ GameOfLife/Game.cs
@@ -0,0 +1,116 @@
+﻿namespace GameOfLife
+{
+    internal class Game
+    {
+        int[,] grid;
+
+        public Game(int width, int height)
+        {
+            grid = new int[width, height];
+        }
+
+        public void SeedGrid(int width, int height)
+        {
+            Random randInt = new Random();
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    int rnd = randInt.Next(0, 2);
+                    grid[x, y] = rnd;
+                }
+            }
+        }
+
+        public void ApplyRules(int width, int height)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    int neighbors = 0;
+                    int left = (x - 1);
+                    int right = (x + 1);
+                    int up = (y - 1);
+                    int down = (y + 1);
+
+                    if ((x != 0 && x < width - 1) && (y != 0 && y < height - 1)) // keep check within bounds of grid array
+                    {
+                        if (grid[left, up] == 1)
+                        {
+                            neighbors++;
+                        }
+
+                        if (grid[x, up] == 1)
+                        {
+                            neighbors++;
+                        }
+
+                        if (grid[right, up] == 1)
+                        {
+                            neighbors++;
+                        }
+
+                        if (grid[left, y] == 1)
+                        {
+                            neighbors++;
+                        }
+
+                        if (grid[right, y] == 1)
+                        {
+                            neighbors++;
+                        }
+
+                        if (grid[left, down] == 1)
+                        {
+                            neighbors++;
+                        }
+
+                        if (grid[x, down] == 1)
+                        {
+                            neighbors++;
+                        }
+
+                        if (grid[right, down] == 1)
+                        {
+                            neighbors++;
+                        }
+                    }
+
+                    if (grid[x, y] == 1 && neighbors == 2 || neighbors == 3)
+                    {
+                        grid[x, y] = 1;
+                    }
+                    else if (grid[x, y] == 0 && neighbors == 3)
+                    {
+                        grid[x, y] = 1;
+                    }
+                    else
+                    {
+                        grid[x, y] = 0;
+                    }
+                }
+            }
+        }
+
+        public void DrawGrid(int width, int height)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    if (grid[x, y] == 1)
+                    {
+                        Console.Write("#" + " ");
+                    }
+                    else
+                    {
+                        Console.Write(" " + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ GameOfLife/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace GameOfLife
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int WIDTH = 20;
+            int HEIGHT = 20;
+            int generations = 10;
+
+            Game gameOfLife = new Game(WIDTH, HEIGHT);
+            gameOfLife.SeedGrid(WIDTH, HEIGHT);
+            gameOfLife.DrawGrid(WIDTH, HEIGHT);
+            Console.WriteLine("Press any key to start simulation");
+            Console.ReadLine();
+            Console.Clear();
+
+            for (int i = 0; i < generations; i++)
+            {
+                gameOfLife.ApplyRules(WIDTH, HEIGHT);
+                gameOfLife.DrawGrid(WIDTH, HEIGHT);
+
+                Console.WriteLine($"generation : {i + 1}");
+
+                Thread.Sleep(500);
+
+                if(i != generations - 1)
+                {
+                    Console.Clear();
+                }  
+            }
+        }
+    }
+}
